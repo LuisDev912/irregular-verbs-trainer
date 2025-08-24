3082b3bd4f3ad1d8ca1877ce4bc791c61cff94d8
Luis Calleja <luiscallejaenrique@gmail.com>
2025-08-24 18:54:13 -0300

    "new feature: add scoring system - when the user answers correctly, they gain a point - when the user answers incorrectly, they lose a point - the current score is displayed after each question - the score cannot go below zero "

--- main.py
+++ main.py
@@ -54,6 +54,7 @@ verbs = {
 }
 
 def Game():
+    points = 0
     while True:
         chosenVerb = choice(list(verbs.keys()))
         userInput = input(f"What's the past simple of {chosenVerb}?: \n------------------------------------------ \n ->").lower().strip()
@@ -61,8 +62,15 @@ def Game():
         #I use != instead of == to check wrong answers first
         if userInput != verbs[chosenVerb]:
             print(f" ❌ Opps, the correct answer is: {verbs[chosenVerb]}") 
+            points -= 1
         else: 
             print("Correct! ☑️") #using emojis is not necessary, but it makes the game more fun
+            points += 1
+
+        if points < 0:
+            points = 0 #this avoids negative points
+
+        print(f"current points: {points}")
 
         if input("\n Do you want to continue? y/n: \n ->").lower().strip() != "y":
             break
