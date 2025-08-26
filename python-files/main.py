6b5422bb51f636dec3fcde72e4798528fd140cd5
Luis Calleja <luiscallejaenrique@gmail.com>
2025-08-26 16:33:13 -0300

    "OOP implementation - Game class added with mainGame method - mainGame method doesn't take any parameters - mainGame method contains the game loop"

--- python-files/main.py
+++ python-files/main.py
@@ -1,26 +1,30 @@
 from random import choice
 from verbsContainer import verbs
 
-def Game():
-    points = 0
-    while True:
-        chosenVerb = choice(list(verbs.keys()))
-        userInput = input(f"What's the past simple of {chosenVerb}?: \n------------------------------------------ \n ->").lower().strip()
 
-        #I use != instead of == to check wrong answers first
-        if userInput != verbs[chosenVerb]:
-            print(f" ❌ Opps, the correct answer is: {verbs[chosenVerb]}") 
-            points -= 1
-        else: 
-            print("Correct! ☑️") #using emojis is not necessary, but it makes the game more fun
-            points += 1
+class Game:
+    def mainGame(_):
+        points = 0
+        while True:
+            chosenVerb = choice(list(verbs.keys()))
+            userInput = input(f"What's the past simple of {chosenVerb}?: \n------------------------------------------ \n ->").lower().strip()
 
-        if points < 0:
-            points = 0 #this avoids negative points
+            #I use != instead of == to check wrong answers first
+            if userInput != verbs[chosenVerb]:
+                print(f" ❌ Opps, the correct answer is: {verbs[chosenVerb]}") 
+                points -= 1
+            else: 
+                print("Correct! ☑️") #using emojis is not necessary, but it makes the game more fun
+                points += 1
 
-        print(f"current points: {points}")
+            if points < 0:
+                points = 0 #this avoids negative points
 
-        if input("\n Do you want to continue? y/n: \n ->").lower().strip() != "y":
-            break
-    print(f"Your final score is: {points} points. Thanks for playing! 👋")
-Game()
+            print(f"current points: {points}")
+
+            if input("\n Do you want to continue? y/n: \n ->").lower().strip() != "y":
+                break
+        print(f"Your final score is: {points} points. Thanks for playing! 👋")
+#this is just the start, because I will implement more OOP concepts later.
+player = Game()
+player.mainGame()
