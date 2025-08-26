912449939f8e0f937b8262e0f6623b2768c739aa
Luis Calleja <luiscallejaenrique@gmail.com>
2025-08-26 17:00:52 -0300

    "intro function added - the function prints mesagges telling what the program is about. - then, it asks if the user wants to continue or not. - if the user types 'y', mainGame function is called. - this is because I will add a menu later."

--- python-files/main.py
+++ python-files/main.py
@@ -1,14 +1,24 @@
 from random import choice
 from verbsContainer import verbs
 
-
 class Game:
-    def mainGame(_):
+    def intro(_):
+        print("Welcome to the Past Simple Verb Game! 🎉\n" 
+            "the instructions are simple:" 
+            "the game will give you a verb in its base form, and you have to type its past simple form.\n" 
+            "For each correct answer, you earn a point. For each wrong answer, you lose a point.\n" 
+            "For example, if the verb is 'go', you should type 'went'.\n" 
+            "You can quit the game anytime by typing 'n' when asked if you want to continue.\n" 
+            "Let's see how good is your knowledgement! Good luck! 🍀\n" 
+            )
+        if input("Do you want to start the game? y/n: \n ->").lower().strip() == "y":
+            Game.mainGame(_)
+            
+    def mainGame(_): #using _ as self because I don't use any parameters or attributes
         points = 0
         while True:
             chosenVerb = choice(list(verbs.keys()))
             userInput = input(f"What's the past simple of {chosenVerb}?: \n------------------------------------------ \n ->").lower().strip()
-
             #I use != instead of == to check wrong answers first
             if userInput != verbs[chosenVerb]:
                 print(f" ❌ Opps, the correct answer is: {verbs[chosenVerb]}") 
@@ -27,4 +37,4 @@ class Game:
         print(f"Your final score is: {points} points. Thanks for playing! 👋")
 #this is just the start, because I will implement more OOP concepts later.
 player = Game()
-player.mainGame()
+player.intro()
