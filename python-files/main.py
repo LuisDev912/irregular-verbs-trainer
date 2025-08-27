ea8b77713fce525fbefde387bc0db050c8cc125a
Luis Calleja <luiscallejaenrique@gmail.com>
2025-08-27 14:49:09 -0300

    "constructor added - now the constructor takes the parameter to prevent the points from being reset to 0 - fixed the typo with the double dot - changed the print statements to be more consistent"

--- python-files/main.py
+++ python-files/main.py
@@ -2,7 +2,10 @@ from random import choice
 from verbsContainer import verbs
 
 class Game:
-    def intro(_):
+    def __init__(self, points):
+        self.points = points
+
+    def intro(self):
         print("Welcome to the Past Simple Verb Game! 🎉\n" 
             "the instructions are simple:" 
             "the game will give you a verb in its base form, and you have to type its past simple form.\n" 
@@ -12,29 +15,28 @@ class Game:
             "Let's see how good is your knowledgement! Good luck! 🍀\n" 
             )
         if input("Do you want to start the game? y/n: \n ->").lower().strip() == "y":
-            Game.mainGame(_)
+            Game.mainGame(self)
             
-    def mainGame(_): #using _ as self because I don't use any parameters or attributes
-        points = 0
+    def mainGame(self): 
         while True:
             chosenVerb = choice(list(verbs.keys()))
             userInput = input(f"What's the past simple of {chosenVerb}?: \n------------------------------------------ \n ->").lower().strip()
             #I use != instead of == to check wrong answers first
             if userInput != verbs[chosenVerb]:
                 print(f" ❌ Opps, the correct answer is: {verbs[chosenVerb]}") 
-                points -= 1
+                self.points -= 1
             else: 
                 print("Correct! ☑️") #using emojis is not necessary, but it makes the game more fun
-                points += 1
+                self.points += 1
 
-            if points < 0:
-                points = 0 #this avoids negative points
+            if self.points < 0:
+                self.points = 0 #this avoids negative self.points
 
-            print(f"current points: {points}")
+            print(f"current points: {self.points}")
 
             if input("\n Do you want to continue? y/n: \n ->").lower().strip() != "y":
                 break
-        print(f"Your final score is: {points} points. Thanks for playing! 👋")
+        print(f"Your final score is: {self.points} points. Thanks for playing! 👋")
 #this is just the start, because I will implement more OOP concepts later.
-player = Game()
+player = Game(0)
 player.intro()
