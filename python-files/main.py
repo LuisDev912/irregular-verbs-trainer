25edec366493b6bf588ccc272c38973a61423001
Luis Calleja <luiscallejaenrique@gmail.com>
2025-08-28 14:39:19 -0300

    "info method added to Game class. - it's used to display verbs and correct/incorrect answers - I created an incorrectAnswers attribute to track wrong answers"

--- python-files/main.py
+++ python-files/main.py
@@ -2,8 +2,13 @@ from random import choice
 from verbsContainer import verbs
 
 class Game:
-    def __init__(self, points):
+    def __init__(self, points = 0, incorrectAnswers = 0):
         self.points = points
+        self.incorrectAnswers = incorrectAnswers
+
+    def info(self):
+        print("The list of the verbs and its answers are: \n", verbs) #this is just for testing purposes, to see the list of verbs
+        print("\n you answered correctly {self.points} times and incorrectly {self.incorrectAnswers} times.\n")
 
     def intro(self):
         print("Welcome to the Past Simple Verb Game! 🎉\n" 
@@ -25,12 +30,13 @@ class Game:
             if userInput != verbs[chosenVerb]:
                 print(f" ❌ Opps, the correct answer is: {verbs[chosenVerb]}") 
                 self.points -= 1
+                self.incorrectAnswers += 1
             else: 
                 print("Correct! ☑️") #using emojis is not necessary, but it makes the game more fun
                 self.points += 1
 
             if self.points < 0:
-                self.points = 0 #this avoids negative self.points
+                self.points = 0 #this avoids negative points
 
             print(f"current points: {self.points}")
 
@@ -38,5 +44,6 @@ class Game:
                 break
         print(f"Your final score is: {self.points} points. Thanks for playing! 👋")
 #this is just the start, because I will implement more OOP concepts later.
-player = Game(0)
-player.intro()
+
+player = Game()
+player.info()
